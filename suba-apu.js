3090a7d7abc313ffd17c4c4b22faac7596e96806
ratulislam46 <arfanratul46@gmail.com>
2026-03-10 15:38:33 +0600

    Solved removed duplicate number from array

--- suba-apu.js
+++ suba-apu.js
@@ -3,3 +3,11 @@ let reverseText = mainText.split("").reverse();
 // console.log(reverseText);
 
 
+function removeDuplicateNumber(number) {
+    console.log("Inside Function:", ...new Set(number))
+    return [...new Set(number)]
+}
+let numbers = [1, 1, 2, 3, 4, 4, 5, 5, 5, 5];
+// console.log(removeDuplicateNumber(numbers));
+
+
