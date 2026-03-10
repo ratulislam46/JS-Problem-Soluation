fc357fc23b79a73e064fb91a9fe793f59398a74a
ratulislam46 <arfanratul46@gmail.com>
2026-03-10 15:38:56 +0600

    Solved prime number check

--- suba-apu.js
+++ suba-apu.js
@@ -11,3 +11,22 @@ let numbers = [1, 1, 2, 3, 4, 4, 5, 5, 5, 5];
 // console.log(removeDuplicateNumber(numbers));
 
 
+
+// Prime numbr check 
+function primeNumberCheck(number) {
+    if (number <= 1) {
+        console.log("it is not prime");
+        return;
+    }
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            console.log("it is not prime");
+            return;
+        }
+    }
+    console.log("prime");
+    return;
+};
+
+let primeNumber = 2;
+// primeNumberCheck(primeNumber)
